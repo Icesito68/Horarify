e2a1bec6876534f4f2efd30153d010b5e9cced4e
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-04-21 09:50:44 +0100

    Add: Estructura inicial para la aplicación
    Eliminamos la vista principal por el login
    Se cambian un par de cosas para la estetica final

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return Inertia::render('welcome');
+// })->name('home');
+
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
