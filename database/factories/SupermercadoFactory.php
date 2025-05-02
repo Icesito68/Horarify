82d66dfc1c738f3af4996a578fbff95c1109b594
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-05-02 12:29:27 +0100

    Add: Centros de un usuario

--- database/factories/SupermercadoFactory.php
+++ database/factories/SupermercadoFactory.php
@@ -25,7 +25,7 @@ class SupermercadoFactory extends Factory
             'Nombre' => fake()->name('[A-Za-z]{20}'),
             'Direccion' => fake()->address(),
             'NIF' => fake()->regexify('^[0-9]{8}[A-Z]{1}'),
-            'users_id' => User::factory(),
+            'user_id' => User::factory(),
         ];
     }
 }
