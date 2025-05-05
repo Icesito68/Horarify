00fa799df3ce17342b53b886560f3dee99d089ce
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-05-05 12:12:52 +0100

    Add: Fuera paginacion de los empleados

--- app/Http/Controllers/Api/EmpleadoController.php
+++ app/Http/Controllers/Api/EmpleadoController.php
@@ -6,10 +6,11 @@ use Orion\Http\Controllers\Controller;
 use App\Models\Empleado;
 use Illuminate\Http\Request;
 use Orion\Concerns\DisableAuthorization;
+use Orion\Concerns\DisablePagination;
 
 class EmpleadoController extends Controller
 {
-    use DisableAuthorization;
+    use DisableAuthorization, DisablePagination;
 
     protected $model = Empleado::class;
 }
