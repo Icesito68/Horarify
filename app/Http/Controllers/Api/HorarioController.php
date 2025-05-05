2dbfb6f6f60865724da5dc78b07176199be68208
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-05-05 10:52:24 +0100

    Add: Fuera paginacion del horario

--- app/Http/Controllers/Api/HorarioController.php
+++ app/Http/Controllers/Api/HorarioController.php
@@ -6,10 +6,11 @@ use Orion\Http\Controllers\Controller;
 use App\Models\Horario;
 use Orion\Http\Requests\Request as OrionRequest;
 use Orion\Concerns\DisableAuthorization;
+use Orion\Concerns\DisablePagination;
 
 class HorarioController extends Controller
 {
-    use DisableAuthorization;
+    use DisableAuthorization, DisablePagination;
 
     protected $model = Horario::class;
 }
